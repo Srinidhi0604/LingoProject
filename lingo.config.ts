ee73b2e24d60f261ff257f55b656d8144d441e4c
Srinidhi0604 <srinidhip06@gmail.com>
2026-02-20 22:26:31 +0530

    Add Lingo.dev compiler configuration

---
+++ lingo.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "@lingo.dev/compiler";
+
+export default defineConfig({
+  defaultLocale: "en",
+  locales: ["en", "kn", "hi"],
+});
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import { withLingo } from "@lingo.dev/compiler/next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: __dirname,
+  },
 };
 
-export default nextConfig;
+export default async function (): Promise<NextConfig> {
+  return await withLingo(nextConfig, {
+    sourceRoot: "./src/app",
+    sourceLocale: "en",
+    targetLocales: ["kn", "hi"],
+    models: "lingo.dev",
+    dev: {
+      usePseudotranslator: false,
+    },
+    widget: false,
+  });
+}
